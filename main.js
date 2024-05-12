9948f09f24c5dab26348dfdeb7546c713f4fa6ba
Muzakka123 <159703338+Muzakka123@users.noreply.github.com>
2024-05-12 16:20:14 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var members = ["muzakka", "maryam", "hanina", "amna", "muneeb",];
+var message = "would you like to learn python";
+console.log(members[0] + " " + message);
+console.log(members[1] + " " + message);
+console.log(members[2] + " " + message);
+console.log(members[3] + " " + message);
+console.log(members[4] + " " + message);
---
+++ main.ts
@@ -0,0 +1,7 @@
+let members: string[] = [`muzakka`,`maryam`,`hanina`,`amna`,`muneeb`,];
+let message: string = "would you like to learn python";
+console.log(members[0] + " " + message);
+console.log(members[1] + " " + message);
+console.log(members[2] + " " + message);
+console.log(members[3] + " " + message);
+console.log(members[4] + " " + message);
